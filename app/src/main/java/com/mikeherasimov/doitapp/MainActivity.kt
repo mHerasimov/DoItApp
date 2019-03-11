c5ba06d8f3f225c21c820baccb129642b04c2637
Oleh Herasimov <oleh.herasimov.triare@gmail.com>
2019-03-11 10:57:42 +0200

    Create basic navigation graph

--- app/src/main/java/com/mikeherasimov/doitapp/MainActivity.kt
+++ app/src/main/java/com/mikeherasimov/doitapp/MainActivity.kt
@@ -2,11 +2,20 @@ package com.mikeherasimov.doitapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var navController: NavController
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        navController = Navigation.findNavController(this, R.id.nav_fragment)
+        setSupportActionBar(toolbar)
     }
+
 }
