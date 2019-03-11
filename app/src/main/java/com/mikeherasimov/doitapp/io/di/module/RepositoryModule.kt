4d49fbbf0ef4f3c154fe5ca16529cb8a221e4925
Oleh Herasimov <oleh.herasimov.triare@gmail.com>
2019-03-11 11:52:12 +0200

    Add authentication

--- app/src/main/java/com/mikeherasimov/doitapp/io/di/module/RepositoryModule.kt
+++ app/src/main/java/com/mikeherasimov/doitapp/io/di/module/RepositoryModule.kt
@@ -12,7 +12,9 @@ import com.mikeherasimov.doitapp.io.db.TaskDao
 import com.mikeherasimov.doitapp.io.db.UserDao
 import dagger.Module
 import dagger.Provides
+import okhttp3.Interceptor
 import okhttp3.OkHttpClient
+import okhttp3.Response
 import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
@@ -38,10 +40,16 @@ class RepositoryModule {
 
     @Provides
     @Singleton
-    fun getOkHttpClient(): OkHttpClient {
+    fun getOkHttpClient(userDao: UserDao): OkHttpClient {
         return OkHttpClient.Builder()
             .addInterceptor(
                 HttpLoggingInterceptor().apply { level = HttpLoggingInterceptor.Level.BASIC })
+            .addInterceptor { chain ->
+                userDao.getUser().observeForever {
+                    addToken(chain, it.token)
+                }
+                addToken(chain, "")
+            }
             .build()
     }
 
@@ -67,6 +75,13 @@ class RepositoryModule {
     @Singleton
     fun getUserRepository(apiService: ApiService, userDao: UserDao, taskDao: TaskDao): UserRepository {
         return UserRepository(apiService, userDao, taskDao)
+    }
+
+    private fun addToken(chain: Interceptor.Chain, token: String): Response {
+        val newRequest = chain.request().newBuilder()
+            .addHeader("bearerAuth", token)
+            .build()
+        return chain.proceed(newRequest)
     }
 
 }
