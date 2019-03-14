ac867b2d77391946774b8bd1ca7860d10ad021be
Oleh Herasimov <oleh.herasimov.triare@gmail.com>
2019-03-14 16:14:52 +0200

    Move network error to base ViewModel class

--- app/src/main/java/com/mikeherasimov/doitapp/ui/base/ScopedViewModel.kt
+++ app/src/main/java/com/mikeherasimov/doitapp/ui/base/ScopedViewModel.kt
@@ -1,5 +1,7 @@
 package com.mikeherasimov.doitapp.ui.base
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
@@ -11,6 +13,10 @@ abstract class ScopedViewModel: ViewModel(), CoroutineScope {
     private val job = Job()
     override val coroutineContext: CoroutineContext
         get() = Dispatchers.Main + job
+
+    protected val _networkError = MutableLiveData<Boolean>(false)
+    val networkError: LiveData<Boolean>
+        get() = _networkError
 
     override fun onCleared() {
         super.onCleared()
